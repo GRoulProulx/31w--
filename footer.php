5726dcd16a417b6c4f686597754923dcc1d7c51d
GRoulProulx <gproulx85@gmail.com>
2024-10-28 13:22:56 -0400

    Début du peaufinage

--- footer.php
+++ footer.php
@@ -1,13 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <footer>
-       <?php wp_footer(); ?>
-    </footer>
+<footer class="pied">
+    <section class="global pied__global">
+        <div>
+            Auteur : Guillaume Rouleau Proulx
+            lien github : https://github.com/GRoulProulx/31w--
+        </div>
+        <div>
+            Petit resumé de l'exercice : Peaufinez le front-end de mon premier site sur Wordpress.
+            Un ajout de quelques elements comme un barre de recherche dans le footer, un ajout d'une image.
+            <img src="" alt="">
+        </div>
+        <div>
+           <?php get_search_form(); ?>
+        </div>
+    </section>
+</footer>
+<?php wp_footer(); ?>
 </body>
+
 </html>
