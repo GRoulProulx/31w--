6840371aeabf8f58ccb1689323f9c446ff102f7e
GRoulProulx <gproulx85@gmail.com>
2024-10-28 21:29:37 -0400

    Exercice-2 Remise!
    Je pense avoir fait quelques erreurs au niveau de git, manque d'expérience!

--- footer.php
+++ footer.php
@@ -1,16 +1,17 @@
 <footer class="pied">
     <section class="global pied__global">
-        <div>
-            Auteur : Guillaume Rouleau Proulx
+        <p>
+            Auteur : Guillaume Rouleau Proulx <br>
             lien github : https://github.com/GRoulProulx/31w--
-        </div>
-        <div>
+        <div class="footer__text">
             Petit resumé de l'exercice : Peaufinez le front-end de mon premier site sur Wordpress.<br>
             Donc, j'ai modifié les couleurs à mon goût, j'ai aussi amiliorer l'enlignement de mes cartes.<br>
             Un ajout de quelques elements comme une barre de recherche dans le footer et un ajout d'une image temporaire.
-            <img class="logo" src="http://placehold.it/350x150" alt="">
+            
         </div>
-        <div>
+        </p>
+        <div class="pied__img">
+            <img class="logo" src="logo.png" alt="">
            <?php get_search_form(); ?>
         </div>
     </section>
