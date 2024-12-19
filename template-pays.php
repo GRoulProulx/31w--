9cf2e50c2b0510c23d50b02cc1fa75eaa0b9d389
GRoulProulx <gproulx85@gmail.com>
2024-12-19 13:20:56 -0500

    Régler bug Champs Perso

--- template-pays.php
+++ template-pays.php
@@ -15,13 +15,14 @@ get_header();
           <article class="principal__article">
             <h5><?php the_title(); ?></h5>
             <p><?php the_content(); ?></p>
+             <p>Voyageurs: <?php the_field('NombreVoyageurs'); ?></p>
+            <p>Date de depart: <?php the_field('Date_depart'); ?></p>
+            <p>Date de retour: <?php the_field('Date_retour'); ?></p>
+            <p>Découvrez les plus belles destinations à travers le monde, classées par pays. Nous avons soigneusement sélectionné les pays les plus populaires pour vous offrir une expérience unique et inoubliable.
+            </p>
             <?php echo do_shortcode('[carrousel]'); ?>
             <?php echo do_shortcode('[extraire_categorie]'); ?>
           </article>
-           <?php
-              $le_pays = get_field('le_pays');
-              $nombre_de_voyageurs = get_field('nombre_de_voyageurs');
-            ?>
         <?php endwhile; ?>
     </div>
   <?php endif ?>
